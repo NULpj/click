07ee8a902dccdcea47acfcc88d3a1f61ee1a1b6b
idk <random.exe.nul@gmail.com>
2025-01-05 17:16:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const url = 'https://script.google.com/macros/s/AKfycbwqnvqnYkBM6z5numP5p1NuDKvPbS6RXQC_ey7woCTHqBmKMlE1-JK-6qP55MldS4UXVw/exec';
-async function incrementClick() {
-    const response = await fetch(url);
+const url = 'https://script.google.com/macros/s/AKfycbwh2bR-5xHgy-KrkY9V2PbpgL6BHNPohAIF2h-Gxsbarggyir5ofQD4oL8b7IvEMiy6oA/exec';
+
+// Fungsi hanya mengambil data awal tanpa increment
+async function fetchInitialClickCount() {
+    const response = await fetch(`${url}?action=get`);
     const data = await response.json();
     document.getElementById('clickCount').innerText = data.clicks;
 }
-incrementClick();
+
+// Fungsi hanya menambah angka ketika tombol diklik
+async function incrementClick() {
+    const response = await fetch(`${url}?action=increment`);
+    const data = await response.json();
+    document.getElementById('clickCount').innerText = data.clicks;
+}
+
+// Ambil data saat halaman dimuat
+fetchInitialClickCount();
